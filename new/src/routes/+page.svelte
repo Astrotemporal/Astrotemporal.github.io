d0e0d5cf13b0ba447f2e152e710a153c3d99eeae
Gregory Sinaga <sinaga.greg@gmail.com>
2023-12-10 23:40:01 -0800

    Initial commit for new portfolio

---
+++ new/src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<!--<script src="../../../node_modules/flowbite/dist/flowbite.js"></script>-->
+<script lang="ts">
+    import ProjectPlacard from "./projectPlacard.svelte";
+
+    let clientInformation = {
+        x: 0,
+        y: 0
+    };
+
+    function grabPointer(event: { clientX: number; clientY: number; }) {
+        clientInformation.x = event.clientX;
+        clientInformation.y = event.clientY;
+    }
+
+    const projects = [
+        {  // Personal Portfolio!
+            title: 'Personal Portfolio',
+            body: 'This currently WIP website is a portfolio of my work. The initial version was written in HTML and CSS, and utilized some Bootstrap for the layout. It has now been extended to utilize Svelte. Find the repository of the initial version <a href="https://github.com/uci-inf-133/a1-personal-portfolio-Astrotemporal">here</a>. (sike, it\'s private)'
+        },
+
+    ]
+</script>
+
+<!---
+Some tutorials to revisit:
+- https://learn.svelte.dev/tutorial/event-modifiers
+- 
+-->
+
+<body on:pointermove={(grabPointer)}>
+<div id="debugInfo">
+    <h1>Debug Information</h1>
+    <p>Pointer: {clientInformation.x} x {clientInformation.y}</p>
+</div>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+It's lit!!!
+
+<div id="projects" class="items-center content-center">
+    {#each projects as project (project.title)} 
+    <ProjectPlacard {...project} />
+    {/each}
+</div>
+</body>
---
+++ new/src/routes/projectPlacard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    import { Card } from 'flowbite-svelte';
+
+    export let title: String;
+    export let body: String;
+
+    const dispatch = createEventDispatcher(); // use for even more interactibility on project placards
+
+
+</script>
+
+<Card href="/projects/{title.replace(/\s/g, "")}" color="primary" id={title.replace(/\s/g, "")}>
+    <h1>{@html title}</h1>
+    <p>{@html body}</p>
+</Card>
